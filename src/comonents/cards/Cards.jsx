986fde7e7fc4e185551fe1871506db1dbda57dc3
[Maxym Kachmaryk] <[maximkachmaryk12@gmail.com]>
2022-09-19 22:48:51 +0300

    create react app

---
+++ src/comonents/cards/Cards.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Card from "../card/card";
+import "./Cards.css";
+
+function Cards() {
+    return <div className="cards-row">
+
+        <Card  cardtitle="africa"/>
+        <Card  cardtitle="Evrope"/>
+        <Card  cardtitle="Ukraine"/>
+       
+        
+
+    </div>
+}
+export default Cards;
